8b1f45c065c7593b41bebc0cda6fc614fa5d2b4e
Jemanesh <jemaninegash@gmail.com>
2019-08-15 09:20:18 +0300

    the icons for the footer

---
+++ test.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+         
+             <link rel="stylesheet"href="style.css">
+             <link rel="stylesheet" href="../viwer/MDB-Free_4.8.2/css/bootstrap.min.css"/>
+             <link rel="stylesheet" href="../viwer/fontawesome/css/all.css"/>
+            
+   </head>
+   <style>
+body{
+    /*background: #262626;*/
+    margin: 0;
+    padding: 0;
+}
+ul
+{
+    display: flex;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left:  50%;
+    transform: translate(-50%,-50%);
+}
+ul li
+{
+    background: #fff;
+    margin: 10px;
+    list-style: none;
+    font-size: 50px;
+    transition: .5spx;
+}
+ul li :nth-child(1)
+{
+    background: #03b5998;  
+  
+}
+
+ul li :nth-child(2)
+{
+    background: #1877f2;
+}    
+ul li :nth-child(3)
+{
+    background: #3b5998; 
+} 
+ ul li :nth-child(4)
+{
+    background: #21759b; 
+}
+ ul li :nth-child(5)    
+{
+     background: #bc2a8d;
+}        
+ ul li :nth-child(6)  
+ {
+     background: #00aced;  
+ }
+    
+  
+ul li:hoevr
+{
+    transform: translateY(-8px);
+    box-shadow: 010px 10px rgba(0,0,0,.5);
+    
+}
+</style>
+   <body>
+       <ul> 
+           <li><a href="#"><i class="fab fa-telegram-plane"></i></a></li>
+           <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+           <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+           <li><a href="#"><i class="fab fa-github-square"></i></a></li>
+           <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+           <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+       </ul>
+   </body>
+        </html>
+                
+    
+        
+        
